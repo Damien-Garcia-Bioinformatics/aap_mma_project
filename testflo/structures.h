eeb22c8c4debb80f93610b7e62b13a220aa5f927
FloGG <gargant@Gargant.U.A.N>
2022-10-10 13:48:02 +0200

    tructures flo

--- testflo/structures.h
+++ testflo/structures.h
@@ -33,13 +33,45 @@ struct input_parse
 
 class Generator
 {
+	// here the parent parses the input into different boolean profiles and stocks the query
 	private :
 		std::string query;
 		bool profile_anchor[100];
 		bool profile_lv2_exp[100];
 		bool profile_lv1_exp[100];
-		bool profile_anchor[100];
-	
-	public :
+		int exp_instance {0};
+		bool profile_diff_lv3exp[100];
 		
+	// the methods here serve to initialise the private variables
+	public :
+		void init(std::string);
+		void get_anchors(std::string);
+		void get_lv2_profile();
+		void get_lv1_profile();
+		void get_diff_exp();
+		// this will 
+		//int get_exp_number(); not to go through the input a high number of time,
+		//we can get the expansion number in the profile_lv1 method
+	
+		// the public variables aims to be transitted to each child for them
+		// to treat the information and generate tops with events in them	
+	
+		//the two anchors that dictate top intervals
+		std::string anchor_child[2];
+		//expansion instructions, 1st element is (), second is <> (can be empty or 0)
+		std::string exp_child[2];
+		
+
+
+		// instances as much childs as ther e are expansions
+		Expansion instance_child();
+	
+}
+
+
+// the child receives the two anchors it needs, and the two expansion instructions
+// (one if no <>)
+class Expansion : Generator
+{
+	
 }
