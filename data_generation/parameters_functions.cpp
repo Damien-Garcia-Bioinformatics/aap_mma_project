b4aeb28b7010b93c702e850493d7f66f316fd241
Damien Garcia <113433305+Damien-Garcia-Bioinformatics@users.noreply.github.com>
2022-11-06 14:58:48 +0100

    Update parameters_functions.cpp

--- data_generation/parameters_functions.cpp
+++ data_generation/parameters_functions.cpp
@@ -16,7 +16,29 @@
 #include <vector>
 
 
-//
-void read_parameters_file() {
+// Extraction of parameters from generation_parameters.txt and creation of parameters structure.
+void read_parameters_file(parameters &param) {
 	std::ofstream file ;
+	file.open("genertion_parameters.txt",ios::out) ;
+	if (file.is_open()) {
+		while (get_line(file, line) {
+			if (line[0] == '#') {
+				continue ;
+			}
+			// Extraction of expression.
+			if (line.substr(0, line.find('=') == "expression") {
+				param.expression = line.substr(line.find('=')) ;
+				continue ;
+			}
+			// Extraction of number of traces to generate.
+			if (line.substr(0, line.find('=') == "expression") {
+				param.nbTraces = line.substr(line.find('=')) ;
+				continue ;
+			}
+			// Extraction of number of traces to generate.
+			if (line.substr(0, line.find('=') == "expression") {
+				param.maxLen = line.substr(line.find('=')) ;
+				continue ;
+			}
+	}
 }
